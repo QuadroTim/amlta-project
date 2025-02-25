cdac25febd962cac82cb259b84511e006ad5c09a
woranov <wor4nov+github@gmail.com>
2025-02-26 00:46:48 +0100

    initial tapas evaluation notebook

--- src/amlta/tapas/retrieve.py
+++ src/amlta/tapas/retrieve.py
@@ -1,5 +1,7 @@
 import collections
+import itertools
 import warnings
+from typing import Literal, cast, overload
 
 import pandas as pd
 import torch
@@ -42,6 +44,26 @@ def generate_tapas_chunks(
         yield batch
 
 
+@overload
+def retrieve_rows_from_chunk(
+    chunk: pd.DataFrame,
+    query: str,
+    *,
+    model: TapasForQuestionAnswering,
+    tokenizer: CustomTapasTokenizer,
+    threshold: float = 0.15,
+    return_probabilities: Literal[True],
+) -> tuple[list[int], str, list[float]]: ...
+@overload
+def retrieve_rows_from_chunk(
+    chunk: pd.DataFrame,
+    query: str,
+    *,
+    model: TapasForQuestionAnswering,
+    tokenizer: CustomTapasTokenizer,
+    threshold: float = 0.15,
+    return_probabilities: Literal[False],
+) -> tuple[list[int], str]: ...
 def retrieve_rows_from_chunk(
     chunk: pd.DataFrame,
     query: str,
@@ -49,6 +71,7 @@ def retrieve_rows_from_chunk(
     model: TapasForQuestionAnswering,
     tokenizer: CustomTapasTokenizer,
     threshold=0.15,
+    return_probabilities=False,
 ):
     inputs = tokenizer(
         table=chunk.drop(columns=["original_index"]),
@@ -60,26 +83,95 @@ def retrieve_rows_from_chunk(
     with torch.no_grad():
         outputs = model(**inputs.to(device))
 
-    predicted_answer_coordinates, predicted_aggregation_indices = (  # type: ignore
-        tokenizer.convert_logits_to_predictions(
-            inputs.to("cpu"),
-            outputs.logits.detach().to("cpu"),
-            outputs.logits_aggregation.detach().to("cpu"),
-            cell_classification_threshold=threshold,
+    if return_probabilities:
+        predicted_answer_coordinates_w_probs, predicted_aggregation_indices = (
+            tokenizer.convert_logits_to_probabilities(
+                inputs.to("cpu"),
+                outputs.logits.detach().to("cpu"),
+                outputs.logits_aggregation.detach().to("cpu"),
+            )
         )
-    )
+        predicted_answer_probabilities = []
+        predicted_answer_coordinates = []
 
-    predicted_answer_coordinates = predicted_answer_coordinates[0]
+        for coords, probs in predicted_answer_coordinates_w_probs[0]:
+            if probs > threshold:
+                predicted_answer_probabilities.append(probs)
+                predicted_answer_coordinates.append(coords)
+    else:
+        predicted_answer_coordinates, predicted_aggregation_indices = (  # type: ignore
+            tokenizer.convert_logits_to_predictions(
+                inputs.to("cpu"),
+                outputs.logits.detach().to("cpu"),
+                outputs.logits_aggregation.detach().to("cpu"),
+                cell_classification_threshold=threshold,
+            )
+        )
+        predicted_answer_probabilities = None
+        predicted_answer_coordinates = predicted_answer_coordinates[0]
+
     predicted_aggregation_index = predicted_aggregation_indices[0]
-
     aggregation = id2aggregation[predicted_aggregation_index]
 
-    row_indices = [
-        int(chunk.iloc[row_index]["original_index"])
-        for row_index, _ in predicted_answer_coordinates
-    ]
+    if return_probabilities:
+        max_predicted_answer_probabilities = []
+        row_indices = []
 
-    return row_indices, aggregation
+        for row, grp in itertools.groupby(
+            zip(
+                predicted_answer_coordinates, cast(list, predicted_answer_probabilities)
+            ),
+            key=lambda x: x[0][0],  # group by row
+        ):
+            row_indices.append(int(chunk.iloc[row]["original_index"]))
+            # store the max probability for each row
+            max_predicted_answer_probabilities.append(max([x[1] for x in grp]))
+
+        predicted_answer_probabilities = max_predicted_answer_probabilities
+
+    else:
+        row_indices = []
+        last_row_index = -1
+
+        for row_index, _ in predicted_answer_coordinates:
+            if row_index != last_row_index:
+                last_row_index = row_index
+                row_indices.append(int(chunk.iloc[row_index]["original_index"]))
+
+    if return_probabilities:
+        return (
+            row_indices,
+            aggregation,
+            cast(list[float], predicted_answer_probabilities),
+        )
+    else:
+        return row_indices, aggregation
+
+
+@overload
+def retrieve_rows(
+    df: pd.DataFrame,
+    query: str,
+    *,
+    shuffle_rows=True,
+    model: TapasForQuestionAnswering,
+    tokenizer: CustomTapasTokenizer,
+    chunk_size: int = 15,
+    threshold=0.15,
+    return_probabilities: Literal[True] = True,
+) -> tuple[list[int], str, list[float]]: ...
+@overload
+def retrieve_rows(
+    df: pd.DataFrame,
+    query: str,
+    *,
+    shuffle_rows=True,
+    model: TapasForQuestionAnswering,
+    tokenizer: CustomTapasTokenizer,
+    chunk_size: int = 15,
+    threshold=0.15,
+    return_probabilities: Literal[False] = False,
+) -> tuple[list[int], str]: ...
 
 
 def retrieve_rows(
@@ -91,26 +183,34 @@ def retrieve_rows(
     tokenizer: CustomTapasTokenizer,
     chunk_size: int = 15,
     threshold=0.15,
+    return_probabilities=False,
 ):
-    row_indices = []
-    aggregation_candidates = collections.Counter()
+    all_row_indices = []
+    all_probabilities = []
+    all_aggregations = collections.Counter()
 
     for batch in generate_tapas_chunks(
         df,
         shuffle_rows=shuffle_rows,
         chunk_size=chunk_size,
     ):
-        row_indices, predicted_aggregation = retrieve_rows_from_chunk(
+        row_indices, predicted_aggregation, *rest = retrieve_rows_from_chunk(
             batch,
             query,
             model=model,
             tokenizer=tokenizer,
             threshold=threshold,
+            return_probabilities=return_probabilities,
         )
 
-        aggregation_candidates[predicted_aggregation] += 1
-        row_indices.extend(row_indices)
+        all_aggregations[predicted_aggregation] += 1
+        all_row_indices.extend(row_indices)
+        if return_probabilities:
+            all_probabilities.extend(rest[0])
 
-    aggregation, _ = aggregation_candidates.most_common(1)[0]
+    aggregation, _ = all_aggregations.most_common(1)[0]
 
-    return row_indices, aggregation
+    if return_probabilities:
+        return all_row_indices, aggregation, all_probabilities
+    else:
+        return all_row_indices, aggregation
